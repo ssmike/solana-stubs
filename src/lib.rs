1deaed0817f1d64d7d49339948fcdd08ca05e740
Mikhail Surin <surinmike@gmail.com>
2025-10-29 12:25:23 +0300

    restore all registers at exit

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,59 @@
-#[unsafe(no_mangle)]
-pub unsafe fn sol_log_(_dst: *mut u8, _src: *const u8, _n: u64) -> u64 {
+#![cfg_attr(target_os="solana", feature(asm_experimental_arch))]
+use core::arch::asm;
+
+macro_rules! syscall_enter {
+    () => {
+        unsafe {
+            asm! ("
+                lddw r0, 0x2FFFF9EC0
+                stxdw [r0 + 0], r1
+                stxdw [r0 + 8], r2
+                stxdw [r0 + 16], r3
+                stxdw [r0 + 24], r4
+                stxdw [r0 + 32], r5
+                ")
+        }
+    }
+}
+
+macro_rules! syscall_exit {
+    ($code: expr) => {
+        unsafe {
+            asm! ("
+                mov64 r0, {ret}
+                lddw r9, 0x2FFFF9EC0
+                ldxdw r1, [r9 + 0]
+                ldxdw r2, [r9 + 8]
+                ldxdw r3, [r9 + 16]
+                ldxdw r4, [r9 + 24]
+                ldxdw r5, [r9 + 32]
+                exit
+                ",
+                ret = in(reg) $code)
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn sol_log_(_dst: *mut u8, _addr: *const u8, _len: u64) -> u64 {
     0
 }
 
-#[unsafe(no_mangle)]
-pub unsafe fn sol_memcpy_(dst: *mut u8, src: *const u8, n: u64) -> u64 {
+#[no_mangle]
+pub unsafe extern "C" fn sol_memcpy_(dst: *mut u8, src: *const u8, n: u64) {
+    syscall_enter!();
+
     for i in 0..n {
         *dst.offset(i as isize) = *src.offset(i as isize);
     }
-    0
+
+    syscall_exit!(0);
 }
 
-#[unsafe(no_mangle)]
-pub unsafe fn sol_memcmp_(s1: *const u8, s2: *const u8, n: u64, result: *mut i32) -> u64 {
+#[no_mangle]
+pub unsafe extern "C" fn sol_memcmp_(s1: *const u8, s2: *const u8, n: u64, result: *mut i32) {
+    syscall_enter!();
+
     let mut cmpresult: i32 = 0;
     for i in 0..n as usize {
         let i = i as u64;
@@ -25,7 +66,7 @@ pub unsafe fn sol_memcmp_(s1: *const u8, s2: *const u8, n: u64, result: *mut i32
     }
     *result = cmpresult;
 
-    0
+    syscall_exit!(0);
 }
 
 #[unsafe(no_mangle)]
